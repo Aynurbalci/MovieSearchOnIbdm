f88535dff8545f4e8b1e061cb6e1edbc114cbcf6
Aynurbalci <64687379+Aynurbalci@users.noreply.github.com>
2022-07-04 20:48:43 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
+import { useState,useEffect } from 'react';
 import './App.css';
 
-const Person=(props)=>{
-    return(
-        <>
-<h1>Name:{props.name}</h1>
-<h2>Last name: {props.lastname}</h2>
-<h2>Age:{props.age}</h2>
 
-
-        </>
-    )
-}
 const App = () => {
-
-    const name = "aynur";
-    const isNameShowing=true;
+const [counter,setCounter]=useState(0);
+useEffect(() => {
+alert("You've changed the counter to" + counter);
+},[counter]);
+   
     return ( 
     <div className = "App" >
-        <Person name={'john'} lastname={'balci'}/>
-         <Person name="aguk"  age={2+2} />
-          <Person/>
-           <Person/>
-        
-
+      <button onClick={()=> setCounter((prevCount)=>prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=> setCounter((prevCount)=>prevCount+1)}>+</button>
+      
+       
         </div>
     );
 }
